5df4813326dd0b368c568ce7b43c5af6489aa263
Dotan Simha <dotansimha@gmail.com>
2016-10-04 21:15:03 +0300

    Step 1.1: Remove boilerplate example files

--- client/main.ts
+++ client/main.ts
@@ -3,7 +3,7 @@ import 'angular2-meteor-polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { Meteor } from "meteor/meteor";
-import { AppModule } from './imports/app';
+import { AppModule } from './imports/app/app.module';
 
 enableProdMode();
 
