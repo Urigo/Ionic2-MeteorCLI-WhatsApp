3b668c422a6d1181a3a1cd8c5e032747fdea81c7
Dotan Simha <dotansimha@gmail.com>
2016-10-04 21:42:32 +0300

    Step 1.9: Basic native functionality added

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Platform } from "ionic-angular";
+import { StatusBar } from "ionic-native";
 import template from './app.component.html';
 
 @Component({
@@ -7,6 +8,11 @@ import template from './app.component.html';
   template
 })
 export class AppComponent {
-  constructor() {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
   }
 }
