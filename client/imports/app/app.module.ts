90823826b467b80d9b9be6c8c9f8028cda910b35
Dotan Simha <dotansimha@gmail.com>
2016-10-04 21:15:03 +0300

    Step 1.1: Remove boilerplate example files

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { METEOR_PROVIDERS } from 'angular2-meteor';
-
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { DemoDataService } from './demo/demo-data.service';
 
 @NgModule({
   // Components, Pipes, Directive
   declarations: [
-    AppComponent,
-    DemoComponent
+    AppComponent
   ],
   // Entry Components
   entryComponents: [
@@ -18,7 +13,7 @@ import { DemoDataService } from './demo/demo-data.service';
   ],
   // Providers
   providers: [
-    DemoDataService
+
   ],
   // Modules
   imports: [
