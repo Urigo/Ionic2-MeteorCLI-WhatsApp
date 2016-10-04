aff7fd37c88a083406fff2289b354a2aa537a448
Dotan Simha <dotansimha@gmail.com>
2016-10-04 21:58:36 +0300

    Step 2.4: Added missing rootPage variable

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from "ionic-angular";
 import { StatusBar } from "ionic-native";
 import template from './app.component.html';
+import {TabsContainerComponent} from "../pages/tabs-container/tabs-container.component";
 
 @Component({
   selector: 'app',
   template
 })
 export class AppComponent {
+  rootPage = TabsContainerComponent;
+
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
