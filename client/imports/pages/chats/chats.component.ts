47eb504e3edf611c083ddb2062a94e376fc7b886
Dotan Simha <dotansimha@gmail.com>
2016-10-05 14:10:10 +0300

    Step 2.6: Added the chats page component

---
+++ client/imports/pages/chats/chats.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+import template from "./chats.component.html"
+
+@Component({
+  selector: "chats",
+  template
+})
+export class ChatsComponent {
+  constructor() {
+
+  }
+}
