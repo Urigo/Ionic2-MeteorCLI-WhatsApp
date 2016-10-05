1e963a38b4ddd24f2fb5544d591399be70a50073
Dotan Simha <dotansimha@gmail.com>
2016-10-05 14:11:22 +0300

    Step 2.8: Use the chats page component in ion-tabs

--- client/imports/pages/tabs-container/tabs-container.component.ts
+++ client/imports/pages/tabs-container/tabs-container.component.ts
@@ -1,16 +1,19 @@
 import {Component} from "@angular/core";
+import {ChatsComponent} from "../chats/chats.component";
 
 @Component({
   selector: "tabs-container",
   template: `
   <ion-tabs>
-    <ion-tab tabTitle="Chats" tabIcon="chatboxes"></ion-tab>
+    <ion-tab [root]="chatsRoot" tabTitle="Chats" tabIcon="chatboxes"></ion-tab>
     <ion-tab tabTitle="Contacts" tabIcon="contacts"></ion-tab>
     <ion-tab tabTitle="Favorites" tabIcon="star"></ion-tab>
   </ion-tabs>
   `
 })
 export class TabsContainerComponent {
+  chatsRoot = ChatsComponent;
+
   constructor() {
 
   }
