9b0470796c022e50bcbc236017c7c5c254a4b42f
Dotan Simha <dotansimha@gmail.com>
2016-10-06 14:38:51 +0300

    Step 4.4: Added the Component to the NgModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -4,19 +4,22 @@ import { IonicApp, IonicModule } from "ionic-angular";
 import {TabsContainerComponent} from "../pages/tabs-container/tabs-container.component";
 import {ChatsComponent} from "../pages/chats/chats.component";
 import {MomentModule} from "angular2-moment";
+import {MessagesPage} from "../pages/chat/messages-page.component";
 
 @NgModule({
   // Components, Pipes, Directive
   declarations: [
     AppComponent,
     TabsContainerComponent,
-    ChatsComponent
+    ChatsComponent,
+    MessagesPage
   ],
   // Entry Components
   entryComponents: [
     AppComponent,
     TabsContainerComponent,
-    ChatsComponent
+    ChatsComponent,
+    MessagesPage
   ],
   // Providers
   providers: [
