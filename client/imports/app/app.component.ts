7b4c1f0361d2ec69386ed0e2dbe5c90318078a34
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-10 13:05:50 +0200

    Step 5.8: Use LoginComponent if user is not logged in

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { Platform } from "ionic-angular";
 import { StatusBar } from "ionic-native";
+import { Meteor } from 'meteor/meteor';
 import template from './app.component.html';
 import {TabsContainerComponent} from "../pages/tabs-container/tabs-container.component";
+import {LoginComponent} from '../pages/auth/login.component';
 
 @Component({
   selector: 'app',
   template
 })
 export class AppComponent {
-  rootPage = TabsContainerComponent;
+  rootPage: any;
 
   constructor(platform: Platform) {
+    this.rootPage = Meteor.user() ? TabsContainerComponent : LoginComponent;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
