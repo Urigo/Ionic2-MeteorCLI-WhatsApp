7df6aed21d8586ea38476b44f874dd4e6624e4a1
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-10 22:27:48 +0200

    Step 7.15: Expose publications

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,8 @@
 import { Main } from './imports/server-main/main';
 import './imports/methods/methods';
+import './imports/publications/chats.publication';
+import './imports/publications/messages.publication';
+import './imports/publications/users.publication';
 import './imports/api/sms';
 
 const mainInstance = new Main();
