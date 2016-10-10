38348342ff92d0fe68aec79dfe1e05b30805be4f
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-10 17:27:19 +0200

    Step 6.11: Replace chats fabrication with users fabrication

--- server/imports/server-main/main.ts
+++ server/imports/server-main/main.ts
@@ -1,66 +1,44 @@
 import {Chats} from "../../../both/collections/chats.collection";
 import {Messages} from "../../../both/collections/messages.collection";
-import * as moment from "moment";
+import {Users} from '../../../both/collections/users.collection';
+import {Accounts} from 'meteor/accounts-base';
 
 export class Main {
   start(): void {
-    if (Chats.collection.find().count()) return;
+    if (Users.collection.find().count()) return;
 
-    let chatId;
-
-    chatId = Chats.collection.insert({
-      title: 'Ethan Gonzalez',
-      picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
-    });
-
-    Messages.collection.insert({
-      chatId: chatId,
-      content: 'You on your way?',
-      createdAt: moment().subtract(1, 'hours').toDate()
-    });
-
-    chatId = Chats.collection.insert({
-      title: 'Bryan Wallace',
-      picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg'
-    });
-
-    Messages.collection.insert({
-      chatId: chatId,
-      content: 'Hey, it\'s me',
-      createdAt: moment().subtract(2, 'hours').toDate()
-    });
-
-    chatId = Chats.collection.insert({
-      title: 'Avery Stewart',
-      picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg'
-    });
-
-    Messages.collection.insert({
-      chatId: chatId,
-      content: 'I should buy a boat',
-      createdAt: moment().subtract(1, 'days').toDate()
-    });
-
-    chatId = Chats.collection.insert({
-      title: 'Katie Peterson',
-      picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg'
-    });
-
-    Messages.collection.insert({
-      chatId: chatId,
-      content: 'Look at my mukluks!',
-      createdAt: moment().subtract(4, 'days').toDate()
-    });
-
-    chatId = Chats.collection.insert({
-      title: 'Ray Edwards',
-      picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg'
-    });
-
-    Messages.collection.insert({
-      chatId: chatId,
-      content: 'This is wicked good ice cream.',
-      createdAt: moment().subtract(2, 'weeks').toDate()
+    [{
+      phone: '+972540000001',
+      profile: {
+        name: 'Ethan Gonzalez',
+        picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
+      }
+    }, {
+      phone: '+972540000002',
+      profile: {
+        name: 'Bryan Wallace',
+        picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg'
+      }
+    }, {
+      phone: '+972540000003',
+      profile: {
+        name: 'Avery Stewart',
+        picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg'
+      }
+    }, {
+      phone: '+972540000004',
+      profile: {
+        name: 'Katie Peterson',
+        picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg'
+      }
+    }, {
+      phone: '+972540000005',
+      profile: {
+        name: 'Ray Edwards',
+        picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg'
+      }
+    }].forEach(user => {
+      Accounts.createUserWithPhone(user);
     });
   }
 }
