a133fcb57abcc1650285d7d7ba8ad7158d52c67c
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-10 13:10:19 +0200

    Step 5.19: Create ProfileComponent

---
+++ client/imports/pages/auth/profile.component.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit} from '@angular/core';
+import {NavController, AlertController} from 'ionic-angular';
+import {Meteor} from 'meteor/meteor';
+import {MeteorObservable} from 'meteor-rxjs';
+import {Profile} from '../../../../both/models/profile.model';
+import {TabsContainerComponent} from '../tabs-container/tabs-container.component';
+
+import template from './profile.component.html';
+import * as style from './profile.component.scss';
+ 
+@Component({
+  selector: 'profile',
+  template,
+  styles: [
+    style.innerHTML
+  ]
+})
+export class ProfileComponent implements OnInit {
+  profile: Profile;
+ 
+  constructor(
+    private navCtrl: NavController,
+    private alertCtrl: AlertController
+  ) {}
+
+  ngOnInit(): void {
+    this.profile = Meteor.user().profile || {
+      name: '',
+      picture: '/ionicons/svg/ios-contact.svg'
+    };
+  }
+ 
+  done(): void {
+    MeteorObservable.call('updateProfile', this.profile).subscribe({
+      next: () => {
+        this.navCtrl.push(TabsContainerComponent);
+      },
+      error(e: Error) {
+        this.handleError(e);
+      }
+    });
+  }
+ 
+  private handleError(e: Error): void {
+    console.error(e);
+ 
+    const alert = this.alertCtrl.create({
+      title: 'Oops!',
+      message: e.message,
+      buttons: ['OK']
+    });
+ 
+    alert.present();
+  }
+}
