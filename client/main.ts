339b93ae645d3d1b0089b4f810ee44b957cad074
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-10 13:05:03 +0200

    Step 5.7: Wait for user if logging in

--- client/main.ts
+++ client/main.ts
@@ -3,10 +3,19 @@ import 'angular2-meteor-polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { Meteor } from "meteor/meteor";
+import { MeteorObservable } from 'meteor-rxjs';
 import { AppModule } from './imports/app/app.module';
 
 enableProdMode();
 
 Meteor.startup(() => {
-   platformBrowserDynamic().bootstrapModule(AppModule);
+  const sub = MeteorObservable.autorun().subscribe(() => {
+    if (Meteor.loggingIn()) return;
+    
+    setTimeout(() => {
+      sub.unsubscribe();
+    });
+    
+    platformBrowserDynamic().bootstrapModule(AppModule);
+  });
 });
