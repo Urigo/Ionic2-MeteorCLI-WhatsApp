9f2d1d91e9af79448dedac7fcf9b26c109637a1b
dotansimha <dotansimha@gmail.com>
2017-01-25 00:17:51 +0200

    Step 12.23: Implement pick, update and set of profile image

--- client/imports/pages/profile/profile.ts
+++ client/imports/pages/profile/profile.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from 'ionic-angular';
 import { MeteorObservable } from 'meteor-rxjs';
+import { Pictures } from '../../../../imports/collections';
 import { Profile } from '../../../../imports/models';
+import { PictureService } from '../../services/picture';
 import { ChatsPage } from '../chats/chats';
 import template from './profile.html';
 
@@ -14,13 +16,37 @@ export class ProfilePage implements OnInit {
 
   constructor(
     private alertCtrl: AlertController,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private pictureService: PictureService
   ) {}
 
   ngOnInit(): void {
     this.profile = Meteor.user().profile || {
       name: ''
     };
+
+    MeteorObservable.subscribe('user').subscribe(() => {
+      this.picture = Pictures.getPictureUrl(this.profile.pictureId);
+    });
+  }
+
+  selectProfilePicture(): void {
+    this.pictureService.select().then((blob) => {
+      this.uploadProfilePicture(blob);
+    })
+      .catch((e) => {
+        this.handleError(e);
+      });
+  }
+
+  uploadProfilePicture(blob: Blob): void {
+    this.pictureService.upload(blob).then((picture) => {
+      this.profile.pictureId = picture._id;
+      this.picture = picture.url;
+    })
+      .catch((e) => {
+        this.handleError(e);
+      });
   }
 
   updateProfile(): void {
