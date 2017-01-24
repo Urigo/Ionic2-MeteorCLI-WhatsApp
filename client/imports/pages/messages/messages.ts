2472049490b1d0532060862f3566f0d6b32017a6
dotansimha <dotansimha@gmail.com>
2017-01-24 22:37:06 +0200

    Step 12.8: Implement the actual send of picture message

--- client/imports/pages/messages/messages.ts
+++ client/imports/pages/messages/messages.ts
@@ -6,6 +6,7 @@ import * as Moment from 'moment';
 import { Observable, Subscription, Subscriber } from 'rxjs';
 import { Messages } from '../../../../imports/collections';
 import { Chat, Message, MessageType, Location } from '../../../../imports/models';
+import { PictureService } from '../../services/picture';
 import { MessagesAttachmentsComponent } from './messages-attachments';
 import { MessagesOptionsComponent } from './messages-options';
 import template from './messages.html';
@@ -29,7 +30,8 @@ export class MessagesPage implements OnInit, OnDestroy {
   constructor(
     navParams: NavParams,
     private el: ElementRef,
-    private popoverCtrl: PopoverController
+    private popoverCtrl: PopoverController,
+    private pictureService: PictureService
   ) {
     this.selectedChat = <Chat>navParams.get('chat');
     this.title = this.selectedChat.title;
@@ -236,10 +238,23 @@ export class MessagesPage implements OnInit, OnDestroy {
           const location = params.selectedLocation;
           this.sendLocationMessage(location);
         }
+        else if (params.messageType === MessageType.PICTURE) {
+          const blob: Blob = params.selectedPicture;
+          this.sendPictureMessage(blob);
+        }
       }
     });
 
     popover.present();
+  }
+
+  sendPictureMessage(blob: Blob): void {
+    this.pictureService.upload(blob).then((picture) => {
+      MeteorObservable.call('addMessage', MessageType.PICTURE,
+        this.selectedChat._id,
+        picture.url
+      ).zone().subscribe();
+    });
   }
 
   getLocation(locationString: string): Location {
