f353f3aebeae458af913035c9f98f5e001f2fd2a
dotansimha <dotansimha@gmail.com>
2017-01-24 22:31:38 +0200

    Step 12.5: Added picture message type

--- imports/models.ts
+++ imports/models.ts
@@ -9,7 +9,8 @@ export interface Profile {
 
 export enum MessageType {
   TEXT = <any>'text',
-  LOCATION = <any>'location'
+  LOCATION = <any>'location',
+  PICTURE = <any>'picture'
 }
 
 export interface Chat {
