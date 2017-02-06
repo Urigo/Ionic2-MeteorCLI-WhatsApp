5d62b36ef2ae72f3bf2ed3bda93acce38d504041
DAB0mB <emanor6@gmail.com>
2017-02-06 19:15:34 -0200

    Step 1.13: Add Ionic to application basis

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
 import template from "./app.html";
 
 @Component({
-  selector: 'my-app',
   template
 })
-export class MyApp {}
+export class MyApp {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      if (platform.is('cordova')) {
+        StatusBar.styleDefault();
+        Splashscreen.hide();
+      }
+    });
+  }
+}
