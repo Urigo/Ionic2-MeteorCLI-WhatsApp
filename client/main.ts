1b582572e438427a6ce8abb2b7fb1d55f08e43ec
DAB0mB <emanor6@gmail.com>
2017-02-06 19:01:27 -0200

    Step 1.8: Prepare application basis

--- client/main.ts
+++ client/main.ts
@@ -1,22 +1,10 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import 'zone.js';
+import 'reflect-metadata';
 
-import './main.html';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Meteor } from 'meteor/meteor';
+import { AppModule } from './imports/app/app.module';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule);
 });
--- declarations.d.ts
+++ declarations.d.ts
@@ -1,1 +1,5 @@
+/*
+  A wildcard module is declared below to allow third party libraries to be used in an app even if they don't
+  provide their own type declarations.
+ */
 declare module '*';
