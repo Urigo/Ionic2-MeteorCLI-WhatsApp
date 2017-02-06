5c28db5563cfe969d9e6a1226bf80622a4e9f122
DAB0mB <emanor6@gmail.com>
2017-02-06 19:15:34 -0200

    Step 1.13: Add Ionic to application basis

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
     MyApp
   ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
   entryComponents: [
     MyApp
   ],
   providers: [
-    { provide: ErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
