4c4e4f30517a3ea0c9fbafe7e751955de00c0587
DAB0mB <emanor6@gmail.com>
2017-02-06 23:37:48 -0200

    Step 2.6: Add stubs for chats objects

--- client/imports/pages/chats/chats.ts
+++ client/imports/pages/chats/chats.ts
@@ -1,11 +1,65 @@
 import { Component } from '@angular/core';
+import * as Moment from 'moment';
+import { Observable } from 'rxjs';
 import template from './chats.html';
 
 @Component({
   template
 })
 export class ChatsPage {
-  constructor() {
+  chats: Observable<any[]>;
 
+  constructor() {
+    this.chats = this.findChats();
+  }
+
+  private findChats(): Observable<any[]> {
+    return Observable.of([
+      {
+        _id: '0',
+        title: 'Ethan Gonzalez',
+        picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
+        lastMessage: {
+          content: 'You on your way?',
+          createdAt: Moment().subtract(1, 'hours').toDate()
+        }
+      },
+      {
+        _id: '1',
+        title: 'Bryan Wallace',
+        picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
+        lastMessage: {
+          content: 'Hey, it\'s me',
+          createdAt: Moment().subtract(2, 'hours').toDate()
+        }
+      },
+      {
+        _id: '2',
+        title: 'Avery Stewart',
+        picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
+        lastMessage: {
+          content: 'I should buy a boat',
+          createdAt: Moment().subtract(1, 'days').toDate()
+        }
+      },
+      {
+        _id: '3',
+        title: 'Katie Peterson',
+        picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
+        lastMessage: {
+          content: 'Look at my mukluks!',
+          createdAt: Moment().subtract(4, 'days').toDate()
+        }
+      },
+      {
+        _id: '4',
+        title: 'Ray Edwards',
+        picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
+        lastMessage: {
+          content: 'This is wicked good ice cream.',
+          createdAt: Moment().subtract(2, 'weeks').toDate()
+        }
+      }
+    ]);
   }
 }
