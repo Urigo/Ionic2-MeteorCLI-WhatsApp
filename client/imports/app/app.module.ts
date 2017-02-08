d63f2cd2bb4becf8978f6688739646d903e0bfd2
DAB0mB <emanor6@gmail.com>
2017-02-08 13:46:15 -0200

    Step 7.9: Added phone service to NgModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -3,6 +3,7 @@ import { MomentModule } from 'angular2-moment';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { ChatsPage } from '../pages/chats/chats'
 import { MessagesPage } from '../pages/messages/messages';
+import { PhoneService } from '../services/phone';
 import { MyApp } from './app.component';
 
 @NgModule({
@@ -22,7 +23,8 @@ import { MyApp } from './app.component';
     MessagesPage
   ],
   providers: [
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    PhoneService
   ]
 })
 export class AppModule {}
