273d6ab45fb644793b2750af9caeb549b355b73e
DAB0mB <emanor6@gmail.com>
2017-02-08 11:33:35 -0200

    Step 4.4: Use server side data

--- client/imports/pages/chats/chats.ts
+++ client/imports/pages/chats/chats.ts
@@ -1,76 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as Moment from 'moment';
 import { Observable } from 'rxjs';
+import { Chats, Messages } from '../../../../imports/collections';
 import { Chat, MessageType } from '../../../../imports/models';
 import template from './chats.html';
 
 @Component({
   template
 })
-export class ChatsPage {
-  chats: Observable<Chat[]>;
+export class ChatsPage implements OnInit {
+  chats;
 
   constructor() {
-    this.chats = this.findChats();
   }
 
-  private findChats(): Observable<Chat[]> {
-    return Observable.of([
-      {
-        _id: '0',
-        title: 'Ethan Gonzalez',
-        picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg',
-        lastMessage: {
-          content: 'You on your way?',
-          createdAt: Moment().subtract(1, 'hours').toDate(),
-          type: MessageType.TEXT
-        }
-      },
-      {
-        _id: '1',
-        title: 'Bryan Wallace',
-        picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg',
-        lastMessage: {
-          content: 'Hey, it\'s me',
-          createdAt: Moment().subtract(2, 'hours').toDate(),
-          type: MessageType.TEXT
-        }
-      },
-      {
-        _id: '2',
-        title: 'Avery Stewart',
-        picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg',
-        lastMessage: {
-          content: 'I should buy a boat',
-          createdAt: Moment().subtract(1, 'days').toDate(),
-          type: MessageType.TEXT
-        }
-      },
-      {
-        _id: '3',
-        title: 'Katie Peterson',
-        picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg',
-        lastMessage: {
-          content: 'Look at my mukluks!',
-          createdAt: Moment().subtract(4, 'days').toDate(),
-          type: MessageType.TEXT
-        }
-      },
-      {
-        _id: '4',
-        title: 'Ray Edwards',
-        picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg',
-        lastMessage: {
-          content: 'This is wicked good ice cream.',
-          createdAt: Moment().subtract(2, 'weeks').toDate(),
-          type: MessageType.TEXT
-        }
-      }
-    ]);
+  ngOnInit() {
+    this.chats = Chats
+      .find({})
+      .mergeMap((chats: Chat[]) =>
+        Observable.combineLatest(
+          ...chats.map((chat: Chat) =>
+            Messages
+              .find({chatId: chat._id})
+              .startWith(null)
+              .map(messages => {
+                if (messages) chat.lastMessage = messages[0];
+                return chat;
+              })
+          )
+        )
+      ).zone();
   }
 
   removeChat(chat: Chat): void {
-    this.chats = this.chats.map<Chat[]>(chatsArray => {
+    this.chats = this.chats.map(chatsArray => {
       const chatIndex = chatsArray.indexOf(chat);
       chatsArray.splice(chatIndex, 1);
 
